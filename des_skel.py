19890048b7dc2a609ac45bd8cbc43a381a08dde1
rawi142 <rawia.ben.amira1997@gmail.com>
2023-10-19 18:36:59 +0200

    derniere version qui marche

--- des_skel.py
+++ des_skel.py
@@ -8,23 +8,24 @@ MASK6 = 0x3f
 MASK28 = 0xfffffff
 MASK32 = 0xffffffff
 
-def sbox(n, a):
+
+def sbox(nb_sbox, entier):
     """
     retourne la valeur de la ligne et la colonne de la SBOX
-    correpondante a a
+    nb_sbox correpondante a l entier en entree
     """
-    dernier = a & 1
-    premier = (a >> 5) << 1
+    dernier = entier & 1
+    premier = (entier >> 5) << 1
     ligne = dernier | premier
-    new_a = a >> 1
-    colonne = new_a & 15
+    new_e = entier >> 1
+    colonne = new_e & 15
     position = ligne * 16 + colonne
-    return td.sBox[n][position]
+    return td.sBox[nb_sbox][position]
 
 
-def S(m_clair):
+def boite(m_clair):
     """
-    retourne la boite S 
+    effectue la boite S
     """
     result = 0
     i = 0
@@ -38,94 +39,71 @@ def S(m_clair):
     return result
 
 
-def CalculKi(cle):
+def permutation(longeur, entree, table):
+    """
+    effectue une permutation d un mot entree selon
+    une certaine table
+    """
+    i = 0
+    sortie = 0
+    while i < longeur:
+        bit = (entree >> (table[i] - 1)) & 1
+        sortie = sortie | (bit << i)
+        i += 1
+    return sortie
+
+
+def calcul_ki(cle):
     """
     genere les 16 cles intermidiaires
     """
-    PC1K = 0
-    cle_i = [0]*16
-    i = 0
-    while i < 56:
-        b = td.PC1[i]-1
-        bit = (cle >> b) & 1
-        tmp = PC1K
-        PC1K = tmp | (bit << i)
-        i += 1
+    PC1K = permutation(56, cle, td.PC1)
     gauche = (PC1K >> 28) & MASK28
     droite = PC1K & MASK28
+    cle_i = [0] * 16
     i = 0
     while i < 16:
         bit_shift = td.LS[i]
-        gauche = ((gauche << bit_shift) & MASK28) | (gauche >> (28 - bit_shift))
-        droite = ((droite << bit_shift) & MASK28) | (droite >> (28 - bit_shift))
+        gauche = ((gauche << bit_shift) & MASK28) | (
+            gauche >> (28 - bit_shift))
+        droite = ((droite << bit_shift) & MASK28) | (
+            droite >> (28 - bit_shift))
         tempo = (gauche << 28) | droite
-        cle_i[i] = 0
-        j = 0
-        ki = 0
-        while j < 48:
-            b = td.PC2[j] -1 
-            bit = (tempo >> b) & 1
-            tmp = ki
-            ki = tmp | (bit <<j )
-            j += 1
-        cle_i[i] = ki
+        cle_i[i] = permutation(48, tempo, td.PC2)
         i += 1
-    return (cle_i)
+    return cle_i
 
-def DES(M, cle):
+
+def standard_des(M, cle):
     """
     effectue le standard DES
     """
-    tempo = 0
-    i = 0
-    while i < 64:
-        tempo |= (((M >> (td.IP[i] -1)) & 1) << i)
-        i = i + 1
+    tempo = permutation(64, M, td.IP)
     L = (tempo >> 32)
     R = tempo & MASK32
     i = 0
-    while i < 16 :
+    while i < 16:
         print(hex(L), hex(R))
         Z = L
         L = R
-        tempo = 0
-        j = 0
-        while j < 48:
-            b = td.E[j] -1
-            bit = (R >> b) & 1
-            tmp = tempo
-            tempo = tmp | (bit << j)
-            j += 1
+        tempo = permutation(48, R, td.E)
         print("e ", hex(tempo))
         tempo ^= cle[i]
         print("xor cle ", hex(tempo), " cle", hex(cle[i]))
-        tempo = S(tempo)
+        tempo = boite(tempo)
         print("s ", hex(tempo))
-        
-        R = 0
-        j = 0
-        while j < 32:
-            b = td.P[j] - 1
-            bit = (tempo >> b) & 1
-            tmp = R
-            R = tmp | (bit << j)
-            j += 1
+        R = permutation(32, tempo, td.P)
         R ^= Z
         print("################################################################################")
         i = i + 1
-    print("ici",hex(L), hex(R))
-    #L, R = R, L
-    O = 0
-    i = 0
-    print("ici",hex(L), hex(R))
+    print("ici", hex(L), hex(R))
+    print("ici", hex(L), hex(R))
     nv_bloc = (R << 32) | L
-    while i < 64:
-        O |= (((nv_bloc >> (td.InvIP[i] -1)) & 1) << i)
-        i = i + 1
-    return hex(O)
+    return hex(permutation(64, nv_bloc, td.InvIP))
+
+
 if __name__ == "__main__":
     K = 0x1123456789abcdef
     M = 0xaaaabbbbccccdddd
-    Ki = CalculKi(K)
-    print(DES(M, Ki))
-
+    Ki = calcul_ki(K)
+    print(standard_des(M, Ki))

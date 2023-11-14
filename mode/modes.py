03a136d24eaa17f5aeaa8b7defed2d2cf8f7ab8f
rawi124 <rawia.ben.amira1997@gmail.com>
2023-11-14 23:51:01 +0100

    finit tous les modes sans test

--- mode/modes.py
+++ mode/modes.py
@@ -1,18 +1,86 @@
+import des_skel as des
+
 def convert_text_bin(texte):
     """
     """
     result = list(format(c, 'b') for c in bytearray(texte, "utf-8"))
     return result
 
-def mode_ecb(texte, cle, taille):
+def decoupage(texte, taille):
     """
     """
-    binaire = convert_text_bin(texte)
-    decoupe = []
     i = 0
     while i < len(binaire) :
         decoupe.append(binaire[i:taille])
         taille += taille
         i += taille
     return decoupe
-print(mode_ecb("raraerzetzetgrfgbfdhrthtryhtjuytj", 0x1123456789abcdef, 10))
+    
+
+def mode_ecb(texte, cle, taille):
+    """
+    """
+    binaire = convert_text_bin(texte)
+    decoupe = decoupage(texte, taille)
+    encrypt = []
+    for el in decoupe :
+        enrypt.append(des.standard_des(el, cle))
+    return encrypt
+
+def mode_cbc(texte, cle, taille, iv):
+    """
+    """
+    binaire = convert_text_bin(texte)
+    decoupe = decoupage(texte, taille)
+    encrypt = []
+    for el in decoupe :
+        tmp = el ^ iv
+        iv = des.standard_des(tmp, cle)
+        encrypt.append(iv)
+    return encrypt
+
+def mode_ctr(texte, cle, taille, compteur):
+    """
+    """
+    binaire = convert_text_bin(texte)
+    decoupe = decoupage(texte, taille)
+    encrypt = []
+    i = 0
+    while i < len(decoupe) :
+        tmp = des.standard_des(compteur[i], cle)
+        encrypt.append(tmp ^ decoupe[i])
+    return encrypt
+
+def mode_cfb(texte, cle, taille, compteur):
+    """
+    """
+    binaire = convert_text_bin(texte)
+    decoupe = decoupage(texte, taille)
+    encrypt = []
+    i = 0
+    while i < len(decoupe) :
+        tmp = des.standard_des(compteur, cle)
+        compteur = tmp ^ decoupe[i]
+        encrypt.append(compteur)
+    return encrypt
+
+
+def mode_ofb(texte, cle, taille, compteur):
+    """
+    """
+    binaire = convert_text_bin(texte)
+    decoupe = decoupage(texte, taille)
+    encrypt = []
+    i = 0
+    while i < len(decoupe) :
+        tmp = des.standard_des(compteur, cle)
+        compteur = tmp ^ decoupe[i]
+        encrypt.append(compteur)
+        compteur = tmp
+    return encrypt
+
+
+    
+    
+    
+    

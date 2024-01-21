9950ea30eb5a744f70ded192159b5786a8c92740
rawi124 <99130108+rawi124@users.noreply.github.com>
2024-01-21 01:15:52 +0100

    Update q8.py

--- rsa/q8.py
+++ rsa/q8.py
@@ -1,5 +1,17 @@
 def calcul_point(k,P,p):
-    while k :
-        k = k >> 1
-
-    bit = k & 1
+    """
+    effectue double and add
+    """
+    n = k
+    cpt = 0
+    while n :
+        n >>= 1
+        cpt += 1
+    Q = P
+    tmp = 1 << (cpt-2)
+    while tmp :
+        Q = double(Q, P)
+        if k & tmp :
+            Q = add(Q, P, p
+        tmp >>= 1
+    return Q 
